52bc3bfd79d6322d6b23c92827bc1872fe3355b3
Martin Thomsen <201800305@post.au.dk>
2020-03-05 12:00:43 +0100

    Små ændringer - f. eks. at lave classes public og noget navngivning

--- Ladeskab/Logfile.cs
+++ Ladeskab/Logfile.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ladeskab
 {
-    class Logfile
+    public class Logfile
     {
     }
 }
--- Ladeskab/RFIDreader.cs
+++ Ladeskab/RFIDreader.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ladeskab
 {
-    class RFIDreader
+   public class RFIDreader
     {
     }
 }
