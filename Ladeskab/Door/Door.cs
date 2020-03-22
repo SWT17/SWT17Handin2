aea6c2ba8c1c0ac55cf3eaef7dee07e007043279
Martin Thomsen <201800305@post.au.dk>
2020-03-22 13:43:01 +0100

    Tilføjet DoorState i door klasse og sat som condition at dør kun kan åbnes ved unlocked state.

---
+++ Ladeskab/Door/Door.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab
+{
+    public class Door : IDoor
+    {
+        // her anvendes to events da de hver især repræsenterer forskellige states
+        // events indeholder ingen yderligere information
+
+        public event EventHandler<DoorOpenEventArgs> DoorOpenEvent;
+        public event EventHandler<DoorClosedEventArgs> DoorClosedEvent;
+        private DoorState _doorState;
+
+
+        private enum DoorState
+        {
+            Locked,
+            Unlocked
+        }
+
+
+        public void OnUserOpensDoor()
+        {
+            if (_doorState == DoorState.Unlocked)
+            {
+                DoorOpenEvent?.Invoke(this, new DoorOpenEventArgs());
+            }
+        }
+
+        public void OnUserClosesDoor()
+        {
+            DoorClosedEvent?.Invoke(this, new DoorClosedEventArgs());
+        }
+
+        public void LockDoor()
+        {
+            _doorState = DoorState.Locked;
+        }
+
+        public void UnlockDoor()
+        {
+            _doorState = DoorState.Unlocked;
+        }
+
+    }
+}
