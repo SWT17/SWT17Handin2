2a0b0f178dea430630964a22e21421f166161cf4
Nicolaj Kvejborg Hansen <au461460@uni.au.dk>
2020-03-25 11:18:19 +0100

    LogFile test af LogDoorUnlocked metode

---
+++ Unittest/LogFileUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Ladeskab;
+
+namespace Unittest
+{
+    [TestFixture]
+
+    public class LogFileUnitTest
+    {
+
+        public void LogDoorUnlocked_SaveID111_fileHasSavedID111()
+        {
+            //Clear all text in the log//
+            File.WriteAllText(System.AppDomain.CurrentDomain.BaseDirectory + "\\Log.txt", String.Empty);
+
+            //Opret uut og id
+            var uut = new Logfile();
+            var id = "111";
+            //Gem i filen med 
+            uut.LogDoorUnlocked(111);
+
+            var fileText = File.ReadAllText(System.AppDomain.CurrentDomain.BaseDirectory + "\\Log.txt");
+
+            StringAssert.Contains(id, fileText);
+
+            File.WriteAllText(System.AppDomain.CurrentDomain.BaseDirectory + "\\Log.txt", String.Empty);
+
+
+
+        }
+
+    }
+}
